0a0f89c2b8b3d8bbea9e378de82392112d726c03
Mathieu Méa <mathieu.mea@gmail.com>
2021-03-08 11:06:07 -0500

    Cleanup

--- src/main/java/org/mtransit/parser/ca_montreal_stm_bus/MontrealSTMBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_montreal_stm_bus/MontrealSTMBusAgencyTools.java
@@ -2,22 +2,18 @@ package org.mtransit.parser.ca_montreal_stm_bus;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.RegexUtils;
+import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.StringUtils;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -27,61 +23,25 @@ import static org.mtransit.parser.StringUtils.EMPTY;
 // http://www.stm.info/sites/default/files/gtfs/gtfs_stm.zip
 public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-montreal-stm-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new MontrealSTMBusAgencyTools().start(args);
 	}
 
-	@Nullable
-	private HashSet<Integer> serviceIds;
-
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating STM bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating STM bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIds != null && this.serviceIds.isEmpty();
+	public String getAgencyName() {
+		return "STM";
 	}
 
 	@NotNull
 	@Override
 	public Integer getAgencyRouteType() {
 		return MAgency.ROUTE_TYPE_BUS;
-	}
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (this.serviceIds != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIds);
-		}
-		return super.excludeTrip(gTrip);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIds);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIds);
-		}
-		return super.excludeCalendar(gCalendar);
 	}
 
 	@Override
@@ -124,8 +84,8 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 		result = P1NUITP2.matcher(result).replaceAll(EMPTY);
 		result = EXPRESS.matcher(result).replaceAll(EXPRESS_REPLACEMENT);
 		result = NAVETTE.matcher(result).replaceAll(NAVETTE_REPLACEMENT);
-		result = Utils.replaceAll(result.trim(), START_WITH_ST, EMPTY);
-		result = Utils.replaceAll(result, SPACE_ST, CleanUtils.SPACE);
+		result = RegexUtils.replaceAllNN(result.trim(), START_WITH_ST, EMPTY);
+		result = RegexUtils.replaceAllNN(result, SPACE_ST, CleanUtils.SPACE);
 		return CleanUtils.cleanLabelFR(result);
 	}
 
@@ -168,14 +128,6 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
-				gTrip.getDirectionIdOrDefault()
-		);
-	}
-
-	@Override
 	public boolean directionFinderEnabled() {
 		return true;
 	}
@@ -189,14 +141,12 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 		return directionHeadSign;
 	}
 
+	@NotNull
 	@Override
-	public int getDirectionType() {
-		return MTrip.HEADSIGN_TYPE_DIRECTION;
-	}
-
-	@Override
-	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		throw new MTLog.Fatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
+	public List<Integer> getDirectionTypes() {
+		return Collections.singletonList(
+				MTrip.HEADSIGN_TYPE_DIRECTION
+		);
 	}
 
 	private static final Pattern STARTS_WITH_RSN_DASH_BOUND_ = Pattern.compile("(^\\d+-[A-Z])");
@@ -216,8 +166,8 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 		stopName = CLEAN_SUBWAY.matcher(stopName).replaceAll(CLEAN_SUBWAY_REPLACEMENT);
 		stopName = CLEAN_SUBWAY2.matcher(stopName).replaceAll(CLEAN_SUBWAY2_REPLACEMENT);
 		stopName = CleanUtils.cleanSlashes(stopName);
-		stopName = Utils.replaceAll(stopName.trim(), START_WITH_ST, StringUtils.EMPTY);
-		stopName = Utils.replaceAll(stopName, SPACE_ST, CleanUtils.SPACE);
+		stopName = RegexUtils.replaceAllNN(stopName.trim(), START_WITH_ST, StringUtils.EMPTY);
+		stopName = RegexUtils.replaceAllNN(stopName, SPACE_ST, CleanUtils.SPACE);
 		stopName = CleanUtils.cleanLabelFR(stopName);
 		StringBuilder resultSB = new StringBuilder();
 		String[] words = stopName.split(SLASH);
@@ -234,14 +184,14 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 
 	private static final String SPACE = " ";
 
-	private static final String PARENTHESE1 = "\\(";
-	private static final String PARENTHESE2 = "\\)";
+	private static final String PARENTHESIS1 = "\\(";
+	private static final String PARENTHESIS2 = "\\)";
 	private static final String SLASH = "/";
-	private static final Pattern CLEAN_SUBWAY = Pattern.compile("(station)([^" + PARENTHESE1 + "]*)" + PARENTHESE1 + "([^" + SLASH + "]*)" + SLASH + "([^"
-			+ PARENTHESE2 + "]*)" + PARENTHESE2, Pattern.CASE_INSENSITIVE);
-	private static final String CLEAN_SUBWAY_REPLACEMENT = "$3 " + SLASH + " $4 " + PARENTHESE1 + "$2" + PARENTHESE2 + "";
+	private static final Pattern CLEAN_SUBWAY = Pattern.compile("(station)([^" + PARENTHESIS1 + "]*)" + PARENTHESIS1 + "([^" + SLASH + "]*)" + SLASH + "([^"
+			+ PARENTHESIS2 + "]*)" + PARENTHESIS2, Pattern.CASE_INSENSITIVE);
+	private static final String CLEAN_SUBWAY_REPLACEMENT = "$3 " + SLASH + " $4 " + PARENTHESIS1 + "$2" + PARENTHESIS2 + "";
 	private static final Pattern CLEAN_SUBWAY2 = Pattern.compile("(station)([^" + SLASH + "]*)" + SLASH + "(.*)", Pattern.CASE_INSENSITIVE);
-	private static final String CLEAN_SUBWAY2_REPLACEMENT = "$3 " + PARENTHESE1 + "$2" + PARENTHESE2 + "";
+	private static final String CLEAN_SUBWAY2_REPLACEMENT = "$3 " + PARENTHESIS1 + "$2" + PARENTHESIS2 + "";
 
 	private static final String CHARS_NO = "no ";
 
