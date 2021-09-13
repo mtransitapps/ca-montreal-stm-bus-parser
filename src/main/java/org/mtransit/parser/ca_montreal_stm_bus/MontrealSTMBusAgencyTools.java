8c14192fce3022f931da12a0317eeb4725d25013
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-13 16:07:22 -0400

    Agency color from routes, cleanup...

--- src/main/java/org/mtransit/parser/ca_montreal_stm_bus/MontrealSTMBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_montreal_stm_bus/MontrealSTMBusAgencyTools.java
@@ -1,5 +1,9 @@
 package org.mtransit.parser.ca_montreal_stm_bus;
 
+import static org.mtransit.commons.Constants.EMPTY;
+import static org.mtransit.commons.Constants.SPACE;
+import static org.mtransit.commons.Constants.SPACE_;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
@@ -16,8 +20,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
-
-import static org.mtransit.parser.StringUtils.EMPTY;
 
 // https://www.stm.info/en/about/developers
 // https://www.stm.info/sites/default/files/gtfs/gtfs_stm.zip
@@ -62,7 +64,7 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 	@NotNull
 	@Override
 	public String getRouteLongName(@NotNull GRoute gRoute) {
-		String gRouteLongName = gRoute.getRouteLongNameOrDefault();
+		final String gRouteLongName = gRoute.getRouteLongNameOrDefault();
 		if (StringUtils.isEmpty(gRouteLongName)) {
 			if (RTS_809.equals(gRoute.getRouteShortName())) {
 				return RLN_809;
@@ -72,59 +74,49 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 		return cleanRouteLongName(gRouteLongName);
 	}
 
-	private static final Pattern EXPRESS = Pattern.compile("(express)", Pattern.CASE_INSENSITIVE);
-	private static final String EXPRESS_REPLACEMENT = " ";
+	private static final Pattern EXPRESS_ = CleanUtils.cleanWord("express");
 
-	private static final Pattern NAVETTE = Pattern.compile("(navette )", Pattern.CASE_INSENSITIVE);
-	private static final String NAVETTE_REPLACEMENT = " ";
+	private static final Pattern NAVETTE_ = CleanUtils.cleanWord("navette");
 
 	@NotNull
 	@Override
 	public String cleanRouteLongName(@NotNull String result) {
 		result = P1NUITP2.matcher(result).replaceAll(EMPTY);
-		result = EXPRESS.matcher(result).replaceAll(EXPRESS_REPLACEMENT);
-		result = NAVETTE.matcher(result).replaceAll(NAVETTE_REPLACEMENT);
+		result = EXPRESS_.matcher(result).replaceAll(SPACE_);
+		result = NAVETTE_.matcher(result).replaceAll(SPACE_);
 		result = RegexUtils.replaceAllNN(result.trim(), START_WITH_ST, EMPTY);
 		result = RegexUtils.replaceAllNN(result, SPACE_ST, CleanUtils.SPACE);
 		return CleanUtils.cleanLabelFR(result);
 	}
 
-	private static final String AGENCY_COLOR = "009EE0";
-
-	@NotNull
-	@Override
-	public String getAgencyColor() {
-		return AGENCY_COLOR;
+	public boolean defaultAgencyColorEnabled() {
+		return true;
 	}
 
-	private static final String COLOR_GREEN = "007339";
-	private static final String COLOR_BLACK = "000000";
-	private static final String COLOR_BLUE = "0060AA";
-	// private static final String COLOR_OR = "FFD700";
-
-	//	private static final List<Long> ROUTES_OR = Arrays.asList(//
-	//			252L, 253L, 254L, 256L, 257L, 258L, 259L, //
-	//			260L, 262L, 263L //
-	//	);
+	private static final String COLOR_GREEN_EXPRESS = "007339";
+	private static final String COLOR_BLACK_NIGHT = "000000";
+	private static final String COLOR_BLUE_REGULAR = "0060AA";
 
 	@Nullable
 	@Override
-	public String getRouteColor(@NotNull GRoute gRoute) {
+	public String getRouteColor(@NotNull GRoute gRoute, @NotNull MAgency agency) {
 		String routeColor = gRoute.getRouteColor();
-		if (AGENCY_COLOR.equalsIgnoreCase(routeColor)) {
+		if (agency.getColor().equalsIgnoreCase(routeColor)) {
 			routeColor = null; // ignore agency color (light blue)
 		}
 		if (StringUtils.isEmpty(routeColor)) {
-			long routeId = getRouteId(gRoute);
-			if (400L <= routeId && routeId <= 499L) {
-				return COLOR_GREEN;
+			final String rln = gRoute.getRouteLongNameOrDefault();
+			final long routeID = getRouteId(gRoute);
+			if (rln.contains("express")
+					|| 400L <= routeID && routeID <= 499L) {
+				return COLOR_GREEN_EXPRESS;
+			} else if (300L <= routeID && routeID <= 399L) {
+				return COLOR_BLACK_NIGHT;
+			} else {
+				return COLOR_BLUE_REGULAR;
 			}
-			if (300L <= routeId && routeId <= 399L) {
-				return COLOR_BLACK;
-			}
-			return COLOR_BLUE;
 		}
-		return super.getRouteColor(gRoute);
+		return super.getRouteColor(gRoute, agency);
 	}
 
 	@Override
@@ -181,8 +173,6 @@ public class MontrealSTMBusAgencyTools extends DefaultAgencyTools {
 		}
 		return resultSB.toString();
 	}
-
-	private static final String SPACE = " ";
 
 	private static final String PARENTHESIS1 = "\\(";
 	private static final String PARENTHESIS2 = "\\)";
